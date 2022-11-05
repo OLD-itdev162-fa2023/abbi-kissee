893551ef6777e9b25f6c305c9255d77d3c5276ec
Abbi Kissee <abbikissee@Abbis-MBP.attlocal.net>
2022-11-05 12:12:34 -0500

    Completed Activity 8

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
